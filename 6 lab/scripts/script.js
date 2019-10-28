d60e7a950974608e0581556c9a0f6070932b95b3
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2019-10-28 16:45:38 +0300

    6.2 update

--- 6 lab/scripts/script.js
+++ 6 lab/scripts/script.js
@@ -4,47 +4,48 @@ const dialog = document.getElementById('dialog');
 const impressionRadios = Array.from(form.elements.impressionInput);
 const places = form.querySelector('#places');
 const countrySelect = form.elements.countrySelect;
+const loadingImg = '../img/loading.png';
 const contriesPlaces = {
     belarus: [
         {
             name: 'Belarus 1',
-            img: '#'
+            img: '../img/belarus-1.png'
         },
         {
             name: 'Belarus 2',
-            img: '#'
+            img: '../img/belarus-2.png'
         },
         {
             name: 'Belarus 3',
-            img: '#'
+            img: '../img/belarus-3.png'
         }
     ],
     italy: [
         {
             name: 'Italy 1',
-            img: '#'
+            img: '../img/italy-1.png'
         },
         {
             name: 'Italy 2',
-            img: '#'
+            img: '../img/italy-2.png'
         },
         {
             name: 'Italy 3',
-            img: '#'
+            img: '../img/italy-3.png'
         }
     ],
     estony: [
         {
             name: 'Estony 1',
-            img: '#'
+            img: '../img/belarus-1.png'
         },
         {
             name: 'Estony 2',
-            img: '#'
+            img: '../img/belarus-1.png'
         },
         {
             name: 'Estony 3',
-            img: '#'
+            img: '../img/belarus-1.png'
         }
     ]
 }
@@ -55,11 +56,16 @@ const liTemplate = document.createElement('li');
 const label = document.createElement('label');
 const input = document.createElement('input');
 const text = document.createElement('span');
+const img = document.createElement('img');
+img.src = loadingImg;
 input.type = 'checkbox';
 label.appendChild(input);
 label.appendChild(text);
 liTemplate.appendChild(label);
-liTemplate.style.display = 'none';
+liTemplate.appendChild(img);
+liTemplate.classList.add('place');
+// Вынеси в классы, балбес
+
 
 // Добавление списка на страницу
 
@@ -68,6 +74,10 @@ for(let country in contriesPlaces){
         place => {
             const li = liTemplate.cloneNode(true);
             li.firstChild.lastChild.innerText = place.name;
+            li.classList.add(country);
+            // const img = document.createElement('img');
+            // img.src = "https://cs5.pikabu.ru/post_img/2019/01/16/5/1547622216182941132.jpg";
+            // img.onload = () => li.children[1].replaceWith(img);
             places.appendChild(li);
         }
     )
@@ -79,18 +89,38 @@ meter.value = impressionRadios.filter(el => el.checked)[0].value;
 // Установка листнеров на каждую радиокнопку
 impressionRadios.forEach(el => el.onchange = e => impressionChange(e.target.value));
 
+const impressionChange = v => {
+    meter.value = v;
+}
+
 // Закрывать диалоговое окно при двойном клике
 dialog.ondblclick = () => {
     dialog.style.display = 'none';
 }
 
-const impressionChange = v => {
-    meter.value = v;
-}
+Array.from(places.children).forEach(
+    place => {
+        place.classList.contains(countrySelect.value) ?
+            place.classList.add('visible') : 
+            place.classList.remove('visible');
+    }
+)
 
 countrySelect.onchange = e => {
-    Array.from(places.children).forEach()
+    Array.from(places.children).forEach(
+        place => {
+            if(place.classList.contains(e.target.value)){
+                place.classList.add('visible');
+            }
+            else{
+                place.classList.remove('visible');
+            }
+        }
+    )
 }
+
+
+const guideFeedback = Array.from(form.elements.guideFeedback.querySelector('ul').children)
 
 form.addEventListener('submit', e => {
     dialog.innerHTML = '';
