73624afd89475d2293440a3432b645a53456f463
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2019-11-06 20:07:40 +0300

    8.5 done

---
+++ 8 lab/scripts/noteBook.js
@@ -0,0 +1,13 @@
+const noteBook = {
+    setData(name, value){
+        localStorage.setItem(name, value);
+    },
+    getData(name){
+        return localStorage.getItem(name) || 'Not found';
+    },
+    deleteData(name){
+        localStorage.removeItem(name);
+    }
+}
+
+export default noteBook;
