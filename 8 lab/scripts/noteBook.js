f866e509ef4975ae14036cd1668aa664608abeaa
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2019-11-08 10:07:18 +0300

    interface update

--- 8 lab/scripts/noteBook.js
+++ 8 lab/scripts/noteBook.js
@@ -1,6 +1,8 @@
 const noteBook = {
     setData(name, value){
-        localStorage.setItem(name, value);
+        if(name && value){
+            localStorage.setItem(name, value);
+        }
     },
     getData(name){
         return localStorage.getItem(name) || 'Not found';
--- 8 lab/scripts/script.js
+++ 8 lab/scripts/script.js
@@ -1,2 +1,21 @@
 import noteBook from './noteBook.js';
-const info = document.getElementById('info');
+const setData = document.getElementById('setData');
+const getData = document.getElementById('getData');
+const deleteData = document.getElementById('deleteData');
+
+setData.onclick = e => {
+    const value = e.target.previousElementSibling.value;
+    const name = e.target.previousElementSibling.previousElementSibling.value;
+    console.log(name, value)
+    noteBook.setData(name, value);
+}
+
+getData.onclick = e => {
+    const name = e.target.previousElementSibling.value;
+    alert(noteBook.getData(name));
+}
+
+deleteData.onclick = e => {
+    const name = e.target.previousElementSibling.value;
+    noteBook.deleteData(name);
+}
